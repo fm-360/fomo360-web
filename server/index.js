a7a92b24eb859bba99223d52ce17999899c5c074
root <root@ip-172-31-27-200.ap-southeast-1.compute.internal>
2018-08-24 11:41:27 +0000

    Allow cross domain access

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ const closeDB = (db) => {
 }
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
     next();
